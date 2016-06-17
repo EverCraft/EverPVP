fba797a2069b97e1b13d4993082809417fa47a18
lesbleu <lesbleu@evercraft.fr>
2016-06-17 17:36:19 +0200

    Update EverPVP

--- src/main/java/fr/evercraft/everpvp/EPMessage.java
+++ src/main/java/fr/evercraft/everpvp/EPMessage.java
@@ -42,40 +42,48 @@ public class EPMessage extends EMessage {
 		UNTAG_ERROR("untag.error", "&cErreur : Le joueur <player> n'est pas en combat."),
 		UNTAGALL_MESSAGE("untagall.message", "&7Tous les joueurs ne sont plus en combat"),
 		
-		// Custom Death Message Player
-		DEATHMESSAGE_PLAYER_ATTACK("deathMessage.player.Attack", "&6<player> &7s'est fait tuer par &6<killer>&7 avec <item>."),
-		DEATHMESSAGE_PLAYER_DECAPITATION("deathMessage.player.decapitation", "&6<player> &7a été décapité par &6<killer>&7 avec <item>."),
-		DEATHMESSAGE_PLAYER_EXPLOSIVE("deathMessage.player.explosive", "&6<player> &7est mort dans une explosion déclenchée par &6<killer>&7."),
-		DEATHMESSAGE_PLAYER_FALL("deathMessage.player.fall", "&6<player> a fait une terrible chute&7 a cause de &6<killer>."),
-		DEATHMESSAGE_PLAYER_FIRE("deathMessage.player.fire", "&6<player> &7a été réduit en cendre par &6<killer>&7."),
-		DEATHMESSAGE_PLAYER_MAGIC("deathMessage.player.magic", "&6<player> &7a été tué par une potion de dégat lancé par &6<killer>&7."),
-		DEATHMESSAGE_PLAYER_PROJECTILE("deathMessage.player.projectile", "&6<player> &7est mort d'une fléche tiré par &6<killer>&7."),
-		DEATHMESSAGE_PLAYER_THORNS("deathMessage.player.thorns", "&6<player> &7a été tué en blessant &6<killer>&7."),
+		// EntityDamageSource Player
+		ENTITY_DAMAGE_PLAYER_ATTACK("entityDamage.player.Attack", "&6<player> &7s'est fait tuer par &6<killer>&7 avec <item>."),
+		ENTITY_DAMAGE_PLAYER_EXPLOSIVE("entityDamage.player.explosive", "&6<player> &7est mort dans une explosion déclenchée par &6<killer>&7."),
+		ENTITY_DAMAGE_PLAYER_FIRE("entityDamage.player.fire", "&6<player> &7a été réduit en cendre par &6<killer>&7."),
+		ENTITY_DAMAGE_PLAYER_MAGIC("entityDamage.player.thorns", "&6<player> &7a été tué en blessant &6<killer>&7."),
 		
-		// Custom Death Message Entity
-		DEATHMESSAGE_ENTITY_ATTACK("deathMessage.entity.attack", "&6<player> &7s'est fait tuer par &6un(e) <monster>&7."),
-		DEATHMESSAGE_ENTITY_CONTACT("deathMessage.entity.contact", "&6<player> &7s'est fait éclaser par un(e) <monster>."),
-		DEATHMESSAGE_ENTITY_EXPLOSIVE("deathMessage.entity.explosive", "&6<player> &7s'est fait tuer dans l'explosion d'&6un(e) <monster>&7."),
-		DEATHMESSAGE_ENTITY_FALL("deathMessage.entity.fall", "&6un(e) <monster> &7a poussé &6<player> et &6<player> a fait une terrible chute&7."),
-		DEATHMESSAGE_ENTITY_MAGIC("deathMessage.entity.magic", "&6<player> &7a été tué par une potion de dégat lancé par &6un(e) <monster>&7."),
-		DEATHMESSAGE_ENTITY_FIRE("deathMessage.entity.fire", "&6<player> &7a été réduit en cendre par &6un(e) <monster>&7."),
-		DEATHMESSAGE_ENTITY_THORNS("deathMessage.entity.thorns", "&6<player> &7s'est fait tuer par &6un(e) <monster>&7."),
-		DEATHMESSAGE_ENTITY_PROJECTILE("deathMessage.entity.projectile", "&6<player> &7est mort d'une flèche tiré par &6un(e) <monster>&7."),
-		DEATHMESSAGE_ENTITY_WITHER("deathMessage.entity.wither", "&6<player> &7est mort atrophié par &6un(e) <monster>&7."),
+		ENTITY_DAMAGE_PLAYER_MAGIC2("entityDamage.player.magic", "&6<player> &7a été tué par une potion de dégat lancé par &6<killer>&7."),
+		ENTITY_DAMAGE_PLAYER_PROJECTILE("entityDamage.player.projectile", "&6<player> &7est mort d'une fléche tiré par &6<killer>&7."),
 		
-		// Custom Death Message Naturally
-		DEATHMESSAGE_NATURALLY_CONTACT("deathMessage.naturally.contact", "&6<player> &7s'est fait piquer à mort."),
-		DEATHMESSAGE_NATURALLY_DROWN("deathMessage.naturally.drown", "&6<player> &7s'est noyé."),
-		DEATHMESSAGE_NATURALLY_EXPLOSIVE("deathMessage.naturally.explosive", "&6<player> &7est mort dans une explosion."),
-		DEATHMESSAGE_NATURALLY_FALL("deathMessage.naturally.fall", "&6<player> &7a fait une chute mortelle."),
-		DEATHMESSAGE_NATURALLY_FIRE("deathMessage.naturally.fire", "&6<player> &7est mort carbonisé."),
-		DEATHMESSAGE_NATURALLY_GENERIC("deathMessage.naturally.generic", "&6<player> &7s'est suicidé."),
-		DEATHMESSAGE_NATURALLY_HUNGER("deathMessage.naturally.hunger", "&6<player> &7est mort de faim."),
-		DEATHMESSAGE_NATURALLY_LIGHTNING("deathMessage.naturally.lightning", "&6<player> &7est mort foudroyé."),
-		DEATHMESSAGE_NATURALLY_MAGIC("deathMessage.naturally.magic", "&6<player> &7s'est fait tué par une potion de dégat II."),
-		DEATHMESSAGE_NATURALLY_PROJECTILE("deathMessage.naturally.projectile", "&6<player> &7s'est fait tué par une flèche."),
-		DEATHMESSAGE_NATURALLY_SUFFOCATE("deathMessage.naturally.suffocate", "&6<player> &7est mort étouffé."),
-		DEATHMESSAGE_NATURALLY_VOID("deathMessage.naturally.void", "&6<player> &7est tombé dans le néant.");
+		// EntityDamageSource Monstre
+		ENTITY_DAMAGE_ENTITY_ATTACK("entityDamage.entity.attack", "&6<player> &7s'est fait tuer par &6un(e) <monster>&7."),
+		ENTITY_DAMAGE_ENTITY_EXPLOSIVE("entityDamage.entity.explosive", "&6<player> &7s'est fait tuer dans l'explosion d'&6un(e) <monster>&7."),
+		ENTITY_DAMAGE_ENTITY_MAGIC("entityDamage.entity.thorns", "&6<player> &7a été tué en blessant &6un(e)<monster>&7."),
+		ENTITY_DAMAGE_ENTITY_FIRE("deathMessage.entity.fire", "&6<player> &7a été réduit en cendre par &6un(e) <monster>&7."),
+		
+		// DamageSource
+		DAMAGE_FALL("damage.fall", "&6<player> &7a fait une terrible chute."),
+		DAMAGE_DROWN("damage.drown", "&6<player> &7s'est noyé."),
+		DAMAGE_EXPLOSIVE("damage.explosive", "&6<player> &7est mort dans une explosion."),
+		DAMAGE_FIRE("damage.fire", "&6<player> &7est mort carbonisé."),
+		DAMAGE_GENERIC("damage.generic", "&6<player> &7s'est suicidé."),
+		DAMAGE_HUNGER("damage.hunger", "&6<player> &7est mort de faim."),
+		DAMAGE_LIGHTNING("damage.lightning", "&6<player> &7est mort foudroyé."),
+		DAMAGE_MAGIC("damage.magic", "&6<player> &7s'est fait tué par une potion."),
+		DAMAGE_PROJECTILE("damage.projectile", "&6<player> &7s'est fait tué par une flèche."),
+		DAMAGE_SUFFOCATE("damage.suffocate", "&6<player> &7a suffoqué dans un mur."),
+		DAMAGE_VOID("damage.void", "&6<player> &7est tombé dans le néant."),
+		
+		//FallingDamage
+		FALLING_DAMAGE_CONTACT("fallingDamage.contact", "&6<player> &7s'est fait écraser par un(e) <entity>."),
+		
+		//BlockDamage
+		BLOCK_DAMAGE_CONTACT("blockDamage.contact", "&6<player> &7s'est fait piquer à mort."),
+		BLOCK_DAMAGE_FIRE("blockDamage.fire", "&6<player> &7a essayé de nager dans la lave."),
+		
+		// Indirect Damag Player
+		INDIRECT_DAMAGE_PLAYER_ATTACK("indirectDamage.player.attack", "&6<player> &7est mort d'une flèche tirée par &6<killer>&7."),
+		INDIRECT_DAMAGE_PLAYER_MAGIC("indirectDamage.player.magic", "&6<player> &7a été tué par une potion lancé par &6<killer>&7."),
+		
+		// Indirect Damage Monstre
+		INDIRECT_DAMAGE_ENTITY_ATTACK("indirectDamage.entity.attack", "&6<player> &7est mort d'une flèche tirée par un(e) &6<monster>&7."),
+		INDIRECT_DAMAGE_ENTITY_MAGIC("indirectDamage.entity.magic", "&6<player> &7a été tué par une potion lancé par un(e) &6<monster>&7.");
 		
 		private final String path;
 	    private final Object french;
